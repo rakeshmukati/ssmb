e7c0ef3bf8210cdfec05587b375f578f5ae07d2e
= <=>
2020-03-22 20:06:33 +0530

    bus data displayed from database

--- public/js/manage-bus-fees.js
+++ public/js/manage-bus-fees.js
@@ -8,49 +8,56 @@ window.onload = function () {
         } else {
             UID = user.uid;
             db = firebase.firestore();
+            loadData();
         }
     });
 }
 
 
 
-    function addBus() {
-        var des = document.getElementById("destination").value;
-        var fee = document.getElementById("busFee").value;
-        db.collection("busFees").add({
-            destination: des,
-            fees: fee
+function addBus() {
+    var des = document.getElementById("destination").value;
+    var fee = document.getElementById("busFee").value;
+    db.collection("busFees").add({
+        destination: des,
+        feePerMonth: fee
+    });
+}
+
+
+function loadData() {
+    db.collection("busFees").onSnapshot(function (querySnapshot) {
+        var buses = [];
+        buses.push({
+            feePerMonth: "Bus Fees",
+            destination: "Destination"
         });
-    }
-
-
-    function addRow() {
-        var table = document.getElementById("students");
-
+        querySnapshot.forEach(function (doc) {
+            let d = doc.data();
+            buses.push({
+                feePerMonth: d.feePerMonth,
+                destination: d.destination
+            });
+            updateTable(buses)
+        });
+    });
+}
+function updateTable(data) {
+    var table = document.getElementById("students");
+    table.innerHTML="";
+    for (a = 0; a < data.length; a++) {
         // Create an empty <tr> element and add it to the 1st position of the table:
         var row = table.insertRow(table.rows.length);
-
         // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-
-        // Add some text to the new cells:
-        cell1.setAttribute("contenteditable", true);
-        cell2.setAttribute("contenteditable", true);
-        cell3.innerHTML = "remove";
-        resetDeleteindex();
+        cell2.innerHTML = data[a].feePerMonth;
+        cell1.innerHTML = data[a].destination;
+        console.log(data[1]);
     }
 
-    function deleteRow(a) {
-        document.getElementById("students").deleteRow(a);
-        resetDeleteindex();
-    }
+}
 
-    function resetDeleteindex() {
-        var rows = document.getElementById("students").rows;
-        console.log("row length = " + rows.length);
-        for (a = 1; a < rows.length; a++) {
-            rows[a].cells[2].setAttribute("onclick", "deleteRow(" + a + ")");
-        }
-    }
+function deleteRow(a) {
+    document.getElementById("students").deleteRow(a);
+}
